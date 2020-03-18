0e7ee780e0b5880ecdeb97f73891f312e5c82f32
Seyi Obaweya <seyi_obaweya@yahoo.com>
2020-03-18 22:21:20 +0800

    added default error handler

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static(__dirname + '/public'))
 
+// default error handling
+app.use((req, res, next) => {
+    res.status(404).send({
+    status: 404,
+    error: 'Not found'
+    })
+   })
+
 // session middleware
 app.use(session({
     secret: 'SOMEVERYSECRETPASSWORD',
